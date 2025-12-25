926b891f5d3f10e0b8ff4959b50640b79792ac25
Hero777 <Hero777@users.noreply.github.com>
2025-12-25 14:22:56 +0000

    Add click for cli

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 # Словарь типов данных: принудительно задаем типы для экономии памяти 
 # и во избежание ошибок интерпретации (особенно важно для Int64, который поддерживает NULL)
@@ -82,19 +83,17 @@ def ingest_data(
 
     print(f'done ingesting to {target_table}')
 
-def main():
-    # Настройки подключения к PostgreSQL (те самые, что мы прописывали в Docker)
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost' # "localhost", так как скрипт запускается снаружи контейнера
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-    
-    # Параметры данных
-    year = 2021
-    month = 1
-    chunksize = 100000
-    target_table = 'yellow_taxi_data'
+@click.command()
+@click.option('--pg-user', default='root', show_default=True, help='Postgres user')
+@click.option('--pg-pass', default='root', show_default=True, help='Postgres password')
+@click.option('--pg-host', default='localhost', show_default=True, help='Postgres host')
+@click.option('--pg-port', default='5432', show_default=True, help='Postgres port')
+@click.option('--pg-db', default='ny_taxi', show_default=True, help='Postgres database')
+@click.option('--year', default=2021, show_default=True, type=int, help='Year of data')
+@click.option('--month', default=1, show_default=True, type=int, help='Month of data (1-12)')
+@click.option('--chunksize', default=100000, show_default=True, type=int, help='CSV chunksize')
+@click.option('--target-table', 'target_table', default='yellow_taxi_data', show_default=True, help='Target DB table')
+def main(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
 
     # Создаем движок SQLAlchemy для работы с Postgres
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
